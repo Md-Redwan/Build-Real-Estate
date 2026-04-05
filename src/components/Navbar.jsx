779f458f3302dc690e379dbcd5053aabfc16fe1e
Md-Redwan <redwanhosen4@gmail.com>
2026-04-05 15:09:42 +0600

    Navbar is complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import { CiSearch } from "react-icons/ci";
+import { LuMenu } from "react-icons/lu";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
+
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div className="pt-8.5">
+      <div className="container rounded-[70px] bg-[#FAFAFA] flex items-center justify-between font-poppins px-7 py-3.75">
+        <img src={logo} alt="Logo" />
+        <div className="hidden lg:block">
+            {/* desktop menu start*/}
+          <ul className="flex items-center gap-8.75 bg-[#FEFEFF] rounded-[40px] px-5 py-5.5">
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Agents</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Listings</a>
+            </li>
+            <CiSearch className="cursor-pointer text-[24px]" />
+          </ul>
+          {/* desktop menu end*/}
+        </div>
+        <div className="hidden lg:flex items-center gap-7.5">
+          <h5>
+            <a href="#">Other services</a>
+          </h5>
+          <button className="px-8.75 py-[20.5px] bg-[#FEFEFF] border-[#BEBEBE] border rounded-[40px] cursor-pointer">
+            Contact us
+          </button>
+        </div>
+        <div className="lg:hidden" onClick={() => setIsMenuOpen((prev) => !prev)}>
+            { isMenuOpen ? (
+              <AiOutlineClose className="cursor-pointer text-[24px]"/>
+            )
+            :
+            (
+              <LuMenu className="cursor-pointer text-[24px]" />
+            )}
+        </div>
+        {/* {isMenuOpen && <p>hello</p>} */}
+      </div>
+      {isMenuOpen && (
+        <div>
+          <div>
+             <ul className="flex items-center gap-8.75 bg-[#FEFEFF] rounded-[40px] px-5 py-5.5">
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Agents</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Listings</a>
+            </li>
+            <CiSearch className="cursor-pointer text-[24px]" />
+          </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
